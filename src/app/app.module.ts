0db3079cc46d4b2e8d8ff6369c099397d5ed97de
colebearski <cole.pedersen@gmail.com>
2021-11-20 14:09:58 -0700

    boilerplate, navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ReviewsComponent } from './components/reviews/reviews.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule, MatToolbarModule } from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ReviewsComponent
   ],
   imports: [
+    AppRoutingModule,
+    BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule
+    MatIconModule,
+    MatToolbarModule
+    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
