0e474de1b4071102649d91555cb39b274a32f41b
colebearski <cole.pedersen@gmail.com>
2021-11-21 11:53:02 -0700

    aggregate rating for romeo and juliet

--- src/app/components/reviews/reviews.component.ts
+++ src/app/components/reviews/reviews.component.ts
@@ -11,12 +11,21 @@ export class ReviewsComponent implements OnInit {
 
   reviews$: Review[];
   p: number = 1;
+  ratingsArr: [] = [];
+  sum: number;
+  average: number;
 
   constructor(private reviewService: ReviewService) { }
 
   ngOnInit() {
-    this.reviewService.getReviews().subscribe((res: Review[]) => {
+    this.reviewService.getReviews().subscribe((res: any[]) => {
       this.reviews$ = res;
+
+      this.reviews$.map(rating => {
+        this.ratingsArr.push(<never>rating.rating);
+        this.sum = this.ratingsArr.reduce((a, b) => a + Math.round(b), 0);
+        this.average = Math.round((this.sum / this.ratingsArr.length) * 100) / 100;
+      });
     });
   }
 }
